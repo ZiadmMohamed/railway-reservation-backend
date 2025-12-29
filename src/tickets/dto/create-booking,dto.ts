21e341342961d5bf96d0fd538c7ed8931f6ebc9d
MonemAdawy <abdoadwy208@gmail.com>
2025-12-29 15:06:13 +0200

    implement ticket booking functionality with validation and repository integration

---
+++ src/tickets/dto/create-booking,dto.ts
@@ -0,0 +1,25 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { Type } from "class-transformer";
+import { ArrayNotEmpty, IsNotEmpty, IsString, ValidateNested } from "class-validator";
+import { i18nValidationMessage } from "nestjs-i18n";
+import { CreatePassenger } from "src/passenger/dto/create-passenger.dto";
+
+export class PassengerBookingInfo {
+  @ApiProperty({ example: 'P12345', description: 'Passenger ID' })
+  @IsString({ message: i18nValidationMessage('validation.isString') })
+  @IsNotEmpty({ message: i18nValidationMessage('validation.required') })
+  passengerId: string;
+}
+
+export class BookTicketsDto {
+  @ApiProperty({ example: 'T12345', description: 'Trip ID' })
+  @IsString({ message: i18nValidationMessage('validation.isString') })
+  @IsNotEmpty({ message: i18nValidationMessage('validation.required') })
+  tripId: string;
+
+  @ApiProperty({ type: [PassengerBookingInfo], description: 'List of passengersId' })
+  @ArrayNotEmpty({ message: i18nValidationMessage('validation.required') })
+  @ValidateNested({ each: true })
+  @Type(() => PassengerBookingInfo)
+  passengers: PassengerBookingInfo[];
+}
---
+++ src/tickets/tickets.service.ts
@@ -0,0 +1,64 @@
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { BookTicketsDto } from '../tickets/dto/create-booking,dto';
+import { PassengerRepository } from 'src/passenger/repositories/passenger.repository';
+import { DB } from 'src/database/drizzle';
+import { InjectDb } from 'src/database/db.provider';
+import { TripsRepo } from 'src/trips/repository/trips.repo';
+import { seats } from 'src/seats/schemas/seats.schema';
+import { tickets } from './schemas/tickets.schema';
+import { eq, and } from 'drizzle-orm';
+import { TicketsRepository } from './repository/tickets.repository';
+
+@Injectable()
+export class TicketsService {
+  constructor(
+    private readonly passengerRepository: PassengerRepository,
+    private readonly ticketsRepository: TicketsRepository,
+    private readonly tripsRepo: TripsRepo,
+  ) {}
+
+  // async bookTickets(dto: BookTicketsDto, userId: string) {
+  async bookTickets(dto: BookTicketsDto) {
+    // dto.passengers.forEach(async (item) => {
+    //   // 1️⃣ check passenger exists & belongs to user
+    //   const passenger = await this.passengerRepository.findOne(
+    //     item.passengerId,
+    //     userId,
+    //   );
+    //   if (!passenger) {
+    //     throw new NotFoundException('Passenger not found');
+    //   }
+    // });
+
+    // 2️⃣ Check if trip exists
+    const trip = await this.tripsRepo.findOne(dto.tripId);
+    if (!trip) {
+      throw new NotFoundException('Trip not found');
+    }
+    console.log('Trip found:', trip);
+
+
+    // ensure tripId and trainId are the correct variables in this scope
+    const availableSeats = await this.ticketsRepository.findAvailableSeats(dto.tripId, trip.trainId);
+    console.log('Available seats:', availableSeats);
+
+    if (availableSeats.length < dto.passengers.length) {
+      throw new BadRequestException('Not enough available seats');
+    }
+
+    const ticketsToInsert = dto.passengers.map((p, index) => ({
+      passengerId: p.passengerId,
+      tripId: dto.tripId,
+      seatId: availableSeats[index].id,
+      price: 50,
+    }));
+    console.log('Tickets to insert:', ticketsToInsert);
+
+    const tickets = await this.ticketsRepository.insertMany(ticketsToInsert);
+    return tickets;
+  }
+}
