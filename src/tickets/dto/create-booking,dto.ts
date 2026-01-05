74f9e73325fde6058c7fb457b7de9852dbaf7aa5
MonemAdawy <abdoadwy208@gmail.com>
2026-01-05 02:47:48 +0200

    updated V3

--- src/tickets/dto/create-booking,dto.ts
+++ src/tickets/dto/create-booking,dto.ts
@@ -1,14 +1,7 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { Type } from 'class-transformer';
 import { ArrayNotEmpty, IsNotEmpty, IsString, IsUUID } from 'class-validator';
 import { i18nValidationMessage } from 'nestjs-i18n';
 
-export class PassengerBookingInfo {
-  @ApiProperty({ example: 'P12345', description: 'Passenger ID' })
-  @IsString({ message: i18nValidationMessage('validation.isString') })
-  @IsNotEmpty({ message: i18nValidationMessage('validation.required') })
-  passengerId: string;
-}
 
 export class BookTicketsDto {
   @ApiProperty({ example: 'T12345', description: 'Trip ID' })
